defcfe04c0e5c0bbd0b6f8e9575a41d8b39cdad9
RuntimeRacer <sge2022@gmail.com>
2025-09-10 08:25:18 +0200

    feat: add testing framework for VNGE Plugin (WIP)

---
+++ tests/Lib/__init__.py
@@ -0,0 +1,3 @@
+# Lib Directory for VNGE Library code
+# For legal reasons this code cannot be included in this repo
+# if you want to run tests, download the latest VNGE version manually and copy the contents of the "Lib" dir into here
